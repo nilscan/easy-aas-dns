a39966d9d5cedd8910472f4c0cc274e451b923d0
nilscan <nilscan31@gmail.com>
2023-07-08 16:55:19 +1200

    Preinstall the latest terraform in the docker image

--- docker/terragrunt-runner/run.sh
+++ docker/terragrunt-runner/run.sh
@@ -11,15 +11,12 @@ print_file() {
   file=$1
   echo "$file:"
   cat $file
+  echo
+  echo
 }
 
 echo "Running terraform module in $TERRAFORM_ROOT_DIR/$TERRAFORM_DIR"
 cd $TERRAFORM_ROOT_DIR/$TERRAFORM_DIR
-
-print_file terragrunt.hcl
-
-# Install terraform using version.tf or latest if not specified
-tfswitch --default $(tfswitch --show-latest)
 
 # Run terragrunt
 terragrunt apply

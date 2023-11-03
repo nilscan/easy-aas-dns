c0bc95db199188a01e7eae97c2db0b9df5dc95c2
nilscan <nilscan31@gmail.com>
2023-11-04 11:53:50 +1300

    Add liveness and readiness probes

--- bin/run_controllers.sh
+++ bin/run_controllers.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-poetry run kopf run -m controllers.audit_controller -m controllers.terraform_resource_controller --log-format=json
+poetry run kopf run -m controllers.audit_controller -m controllers.terraform_resource_controller --log-format=json --liveness=http://0.0.0.0:8080/healthz
